b756a7bbc54e58f671dfef179e41e6a3a58151d5
Muhammad Hasnain Naeem <k253029@nu.edu.pk>
2025-10-20 15:30:05 +0500

    Create code.c

---
+++ code.c
@@ -0,0 +1,71 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    
+    // LAB 7 Q1
+    
+    // int length;
+    // printf("Enter the number of elements: ");
+    // scanf("%d", &length);
+    
+    // if (length < 1){
+    //     printf("\nWrong Input!");
+    //     return 1;
+    // }
+    
+
+    // int arr[length], sum=0;
+    // for(int i=0; i<length; i++){
+    //     printf("\nEnter element number %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    //     sum+=arr[i];
+    // }
+    // printf("\nTotal Sum:\t%d", sum);
+    
+    // LAB 7 Q2
+    
+    // int length;
+    // printf("Enter the number of elements: ");
+    // scanf("%d", &length);
+    
+    // if (length < 1){
+    //     printf("\nWrong Input!");
+    //     return 1;
+    // }
+    
+
+    // int arr[length];
+    // for(int i=0; i<length; i++){
+    //     printf("\nEnter element number %d: ", i+1);
+    //     scanf("%d", &arr[i]);
+    // }
+    // for(int i=length-1; i>=0; i--){
+    //     printf("%d ", arr[i]);
+    // }
+    
+    // LAB 8 Q1
+    // int i=0;
+    // int j=0;
+    
+    // for(i; i<11; i++){
+    //     for(j=0; j<i; j++){
+    //         printf("O");
+    //     }
+    //     printf("\n");
+    // }
+    
+    // LAB 8 Q2
+    
+    // int n;
+    // printf("Enter N to display its multiplication table: ");
+    // scanf("%d", &n);
+    
+    // for(int i=1; i<11; i++){
+    //     printf("\n%d X %d = %d",n,i,n*i);
+    // }
+    
+
+
+    return 0;
+}
